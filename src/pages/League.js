9486209d5de1a874eff45623b291612f6712b636
Carlisle Payne <87280475+CPayne617@users.noreply.github.com>
2021-09-20 22:21:01 -0400

    commit out code to get app to show up on heroku
    commit out code to get app to show up on heroku

--- src/pages/League.js
+++ src/pages/League.js
@@ -1,81 +1,39 @@
-// import "./styles.css";
-// import {useEffect, useState} from 'react'
+import { useEffect, useState } from "react";
+import SearchBar from "../components/SearchBar"
+// import "../SearchBar.css";
 
-// export default function App() {
-//   const [players, setPlayers] = useState([
-//     'teo',
-//     'carlisle',
-//     'titus',
-//     'zamira',
-//     'tory'
-//   ])
-//   const [renderPlayers, setRenderPlayers] = useState(players)
+// const [playerList, setPlayerList] = useState([
+//     player.name
+// ])
+//   const [renderPlayerList, setRenderPlayerList] = useState(playerList)
 //   const [userInput, setUserInput] = useState('')
 
 //   const handleChange = (e) => {
 //     console.log('we are typing', e.target.value)
 //     setUserInput(e.target.value)
-//     let player = players.filter((player) => {
+//     let player = playerList.filter((player) => {
 //       return player.includes(e.target.value)
 //     })
 //     if(player) {
-//       setRenderPlayers(player)
+//       setRenderPlayerList(player)
 //     } else {
-//       setRenderPlayers(players)
+//       setRenderPlayerList(playerList)
 //     }
-//   }
+//   };
 
 //   return (
-//     <div className="App">
-//       <h1>Hello CodeSandbox</h1>
+//     <div className="League">
+//       <h1>League Players</h1>
 //       <form>
 //         <input value={userInput} onChange={handleChange} />
 //         <button type="submit">Submit</button>
 //       </form>
 
-//       {renderPlayers.map((player) => 
+//       {renderPlayerList.map((player) => 
 //         <h1>{player}</h1>
 //       )}
 //     </div>
 //   );
-// }
-
-import { useEffect, useState } from "react";
-import SearchBar from "../components/SearchBar"
-// import "../SearchBar.css";
-
-const [playerList, setPlayerList] = useState([
-    player.name
-])
-  const [renderPlayerList, setRenderPlayerList] = useState(playerList)
-  const [userInput, setUserInput] = useState('')
-
-  const handleChange = (e) => {
-    console.log('we are typing', e.target.value)
-    setUserInput(e.target.value)
-    let player = playerList.filter((player) => {
-      return player.includes(e.target.value)
-    })
-    if(player) {
-      setRenderPlayerList(player)
-    } else {
-      setRenderPlayerList(playerList)
-    }
-  };
-
-  return (
-    <div className="League">
-      <h1>League Players</h1>
-      <form>
-        <input value={userInput} onChange={handleChange} />
-        <button type="submit">Submit</button>
-      </form>
-
-      {renderPlayerList.map((player) => 
-        <h1>{player}</h1>
-      )}
-    </div>
-  );
 
 
 
